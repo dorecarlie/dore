7a303f82bff4aa390373a306b9d43a959034c328
dorecarlie <dorecarlie@gmail.com>
2026-05-07 23:02:41 -0400

    add creare roue for item.js

--- server/routes/item.js
+++ server/routes/item.js
@@ -2,7 +2,9 @@ const express = require("express")
 const router = express.Router()
 const User = require("../models/item")
 
-router.get('/getAllItems', async (req, res) => {
+router
+
+.get('/getAllItems', async (req, res) => {
     try {
         const items = await Item.getAllItems()
         res.send(items)
@@ -10,5 +12,18 @@ router.get('/getAllItems', async (req, res) => {
         res.status(401).send({message: err.message})
     }
 })
+
+.post('/create', async (req, res) => {
+    try{
+        const newItem = await Item.createItem(req.body)
+        res.send(newItem)
+    } catch(err){
+        res.status(401).send({message: err.message})
+    }
+})
+
+
+
+
 
 module.exports = router
