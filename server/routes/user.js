afa9841a838327ec53bca38689fde59a899f251b
dorecarlie <dorecarlie@gmail.com>
2026-05-07 22:59:42 -0400

    fix the placement for router to work

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,7 +2,9 @@ const express = require("express")
 const router = express.Router()
 const User = require("../models/user")
 
-router.get('/getAllUsers', async (req, res) => {
+router
+
+.get('/getAllUsers', async (req, res) => {
     try {
         const users = await User.getAllUsers()
         res.send(users)
